03be2dadf32576d1a2bbdccb7fd98b5e7d302451
Emil Todorinov <85342866+etodorinov@users.noreply.github.com>
2022-08-15 14:33:24 +0300

    Add files via upload

---
+++ React/src/App.js
@@ -0,0 +1,19 @@
+import "./App.css";
+
+import { PeopleProvider } from "./context/PeopleContext";
+
+import { PeopleList } from "./components/PeopleList";
+import { SearchInputAndCriteria } from "./components/SearchInputAndCriteria";
+
+function App() {
+  return (
+    <PeopleProvider>
+      <div className="App">
+        <SearchInputAndCriteria />
+        <PeopleList />
+      </div>
+    </PeopleProvider>
+  );
+}
+
+export default App;
